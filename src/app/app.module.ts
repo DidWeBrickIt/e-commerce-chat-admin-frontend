f63c6c13bf8540e68b9982e517de2e7d3b9828ba
Sarah-C-F <Sarah_Farrens@ymail.com>
2022-09-21 11:05:52 -0700

    refactored component and created service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ChatboxComponent } from './chatbox/chatbox.component';
+import { ChatboxComponent } from './components/chatbox/chatbox.component';
 
 @NgModule({
   declarations: [
