aaf58e667432904896ef413f775faaac478b959c
Sarah-C-F <Sarah_Farrens@ymail.com>
2022-09-21 12:23:18 -0700

    updates

--- src/app/components/chatbox/chatbox.component.ts
+++ src/app/components/chatbox/chatbox.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { re } from 'mathjs';
+import { Message } from 'src/app/models/Message';
+import { SendMessageService } from 'src/app/services/send-message/send-message.service';
+
 
 @Component({
   selector: 'app-chatbox',
@@ -10,6 +14,20 @@ export class ChatboxComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    @Input() inMessage: Message;
+    let text : string = "hello";
+
+  SendMessageService() {
+    let message:Message = {message: text};
+    this.send-message.service.sendMessage(message).subscribe(
+      (response : Message) => {
+        console.log(response);
+      }
+    )
+  }
+
   }
 
 }
+
